677259ee8482105788e3b9ca1f1f87b917b39b0b
dieuts <dieutx90@gmail.com>
2022-10-25 20:43:55 +0700

    adas

--- install-taraxa.sh
+++ install-taraxa.sh
@@ -22,6 +22,6 @@ sudo docker-compose up -d
 
 sleep 3
 
-docker exec taraxa_compose-node-1 cat /opt/taraxa_data/conf/wallet.json
+docker exec taraxa_compose_node_1 cat /opt/taraxa_data/conf/wallet.json
 
-docker exec taraxa_compose-node-1 taraxa-sign sign --wallet /opt/taraxa_data/conf/wallet.json
+docker exec taraxa_compose_node_1 taraxa-sign sign --wallet /opt/taraxa_data/conf/wallet.json
