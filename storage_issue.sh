6cb4ebd0ce7e11e6f10881f2bd0792a7ae9c939b
Dieu Tran <dangtc94@gmail.com>
2023-12-16 16:49:41 +0700

    asd

--- storage_issue.sh
+++ storage_issue.sh
@@ -12,7 +12,11 @@ sleep 1
 sed -i 's/"db_max_snapshots" : 1/"db_max_snapshots" : 0/gi' ./config/testnet.json
 sed -i 's/"db_snapshot_each_n_pbft_block" : 10000/"db_snapshot_each_n_pbft_block" : 0/gi' ./config/testnet.json
 
-cd /var/lib/docker/volumes/${folderdata}/_data/db && rm -rf !("db"|"state_db")
+#cd /var/lib/docker/volumes/${folderdata}/_data/db && rm -rf !("db"|"state_db")
+#exec('bash -c "shopt -s extglob && cd /var/lib/docker/volumes/'"$folderdata"'/_data/db && rm -rf !("db"|"state_db")')
+
+cd /var/lib/docker/volumes/${folderdata}/_data/db
+find -maxdepth 1 ! -name db ! -name state_db ! -name . -exec rm -rv {} \;
 
 sleep 1
 
